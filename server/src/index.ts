a3f7160a4a4b6e4b1e9c40703a650ab3113abae2
amylixing <amylixing@hotmail.com>
2025-05-01 14:07:15 +0800

    很好

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,17 +12,9 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// 静态文件服务
-app.use(express.static(path.join(__dirname, '../../dist')));
-
 // API路由
 app.get('/api', (req, res) => {
   res.json({ message: '先问AI API 服务运行中' });
-});
-
-// SPA路由处理
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../dist/index.html'));
 });
 
 // 启动服务器
